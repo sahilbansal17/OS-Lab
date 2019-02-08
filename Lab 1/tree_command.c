e53be3b10f969a7565988b36e6131943fab8f73e
Sahil <shlbnsl843@gmail.com>
2019-02-08 15:10:21 +0530

    Complete dfs_tree function

--- Lab 1/tree_command.c
+++ Lab 1/tree_command.c
@@ -3,7 +3,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-void dfs_tree(char *path, char *parent) {
+void print_file (char *name, int level) {
+	for (int i = 0; i < level; i ++) {
+		printf("|   ");
+	}
+	if (level != -1)
+		printf("|-- ");
+	printf("%s\n", name);
+}
+
+void dfs_tree(char *path, int level) {
 	struct dirent *de;
 
 	DIR *dr = opendir(path);
@@ -12,26 +21,61 @@ void dfs_tree(char *path, char *parent) {
 		printf("Could not open the directory: %s\n", path);
 	}
 	else {
+		if (level == 0)
+			print_file(path, -1);
 		while ((de = readdir(dr)) != NULL) {
-			if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) {
+			if (de->d_name[0] == '.') {
 				continue;
 			}
 			if (de->d_type == DT_DIR) {
-				char *new_path = (char *)malloc(sizeof(char)*255);
-				strcat(new_path, de->d_name);
+				char *new_path = (char *)malloc(sizeof(char)*255 + strlen(path));
+				strcpy(new_path, path);
 				strcat(new_path, "/");
-				printf("%s\n", new_path);
+				strcat(new_path, de->d_name);
+				print_file(de->d_name, level);
+				dfs_tree(new_path, level + 1);
 			}
 			else {
-				printf("%s\n", de->d_name);
+				print_file(de->d_name, level);
 			}
 		}
 	}
 	closedir(dr);
 }
 
-int main () {
+void bfs_tree(char *path, int level) {
 
-	dfs_tree(".", ".");
+}
+
+int main (int argc, char *argv[]) {
+
+	if (argc == 1) {
+		// by default dfs
+		dfs_tree(".", 0);
+	}
+	if (argc == 2) {
+		// whether bfs or dfs
+		if (strcmp(argv[1], "-d") == 0) {
+			dfs_tree(".", 0);
+		}
+		else if (strcmp(argv[1], "-b") == 0) {
+			bfs_tree(".", 0);
+		}
+		else {
+			printf("Usage of the program: ./tree [-d | -b] [path]\n");
+		}
+	}
+	if (argc == 3) {
+		// whether bfs or dfs as well as path given
+		if (strcmp(argv[1], "-d") == 0) {
+			dfs_tree(argv[2], 0);
+		}
+		else if (strcmp(argv[1], "-b") == 0) {
+			bfs_tree(argv[2], 0);
+		}
+		else {
+			printf("Usage of the program: ./tree [-d | -b] [path]\n");
+		}
+	}
 	return 0;
 }
