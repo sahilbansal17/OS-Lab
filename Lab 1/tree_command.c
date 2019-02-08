b422d89c07296c83a329cea338da5547534beb3d
Sahil <shlbnsl843@gmail.com>
2019-02-08 19:03:14 +0530

    Complete bfs_tree function

--- Lab 1/tree_command.c
+++ Lab 1/tree_command.c
@@ -43,8 +43,49 @@ void dfs_tree(char *path, int level) {
 	closedir(dr);
 }
 
-void bfs_tree(char *path, int level) {
+void bfs_tree(char *path[], int count, int level) {
+	struct dirent *de;
 
+	char *next_level[1024];
+	int current = 0;
+
+	for (int i = 0; i < count; i ++) {
+		if (level == 0) {
+			print_file(path[i], -1);
+		}
+		DIR *dr = opendir(path[i]);
+		if (dr == NULL) {
+			printf("Could not open the directory: %s\n", path[i]);
+		}
+		else {
+			while ((de = readdir(dr)) != NULL) {
+				if (de->d_name[0] == '.') {
+					continue;
+				}
+				if (de->d_type == DT_DIR) {
+					// store
+					next_level[current] = (char *)malloc(sizeof(char)*255 + strlen(path[i]));
+					strcpy(next_level[current], path[i]);
+					strcat(next_level[current], "/");
+					strcat(next_level[current], de->d_name);
+					current ++;
+				}
+				else {
+					char *disp;
+					disp = (char *)malloc(sizeof(char) * (strlen(path[i]) + 1 + strlen(de->d_name)));
+					strcat(disp, path[i]);
+					strcat(disp, "/");
+					strcat(disp, de->d_name);
+					print_file(disp, level);
+				}
+			}
+			closedir(dr);
+		}
+	}
+
+	if (current != 0) {
+		bfs_tree(next_level, current, level + 1);
+	}
 }
 
 int main (int argc, char *argv[]) {
@@ -59,7 +100,11 @@ int main (int argc, char *argv[]) {
 			dfs_tree(".", 0);
 		}
 		else if (strcmp(argv[1], "-b") == 0) {
-			bfs_tree(".", 0);
+			char **paths;
+			paths = (char **)malloc(1 * sizeof(char *));
+			paths[0] = (char *)malloc(sizeof(char) * 255);
+			strcpy(paths[0], ".");
+			bfs_tree(paths, 1, 0);
 		}
 		else {
 			printf("Usage of the program: ./tree [-d | -b] [path]\n");
@@ -71,7 +116,11 @@ int main (int argc, char *argv[]) {
 			dfs_tree(argv[2], 0);
 		}
 		else if (strcmp(argv[1], "-b") == 0) {
-			bfs_tree(argv[2], 0);
+			char **paths;
+			paths = (char **)malloc(1 * sizeof(char *));
+			paths[0] = (char *)malloc(sizeof(char) * 255);
+			strcpy(paths[0], argv[2]);
+			bfs_tree(paths, 1, 0);
 		}
 		else {
 			printf("Usage of the program: ./tree [-d | -b] [path]\n");
